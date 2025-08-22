c0d1a2d6ca94d9b1bcb94cf39662db4005572a24
xenon <ofek171202@gmail.com>
2025-08-22 15:11:02 +0300

    fix bad any cast

--- pipeline_tools/src/flow/blocks/Source.hpp
+++ pipeline_tools/src/flow/blocks/Source.hpp
@@ -12,7 +12,10 @@ namespace pt::flow {
         virtual std::optional<Out> process() = 0;
 
         std::any process_any(const std::any&) override {
-            return process();
+            auto result = process();
+            if (result.has_value())
+                return std::any(result.value());
+            return {};
         }
     };
 }
