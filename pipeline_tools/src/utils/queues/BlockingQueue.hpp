9a064cfa8f6e8b404f4d41a96dd21dfb32e4c743
mkmagic <michaelkanon1@gmail.com>
2025-12-06 11:20:49 +0200

    Fix queus for movable types and add tests for all queue types (#7)

--- pipeline_tools/src/utils/queues/BlockingQueue.hpp
+++ pipeline_tools/src/utils/queues/BlockingQueue.hpp
@@ -5,5 +5,6 @@
 
 namespace pt::queues {
 template<typename T>
-class BlockingQueue final: public MoodycamelQueue<moodycamel::BlockingReaderWriterQueue<T>, T> {};
-}
+class BlockingQueue final :
+    public MoodycamelQueue<T, moodycamel::BlockingReaderWriterQueue<T>> {};
+}   // namespace pt::queues
