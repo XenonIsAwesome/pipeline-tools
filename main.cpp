7faf4570dfaa277bf06c1ce96703c98a68661206
xenon <ofek171202@gmail.com>
2025-12-06 11:21:13 +0200

    automated docker testing

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include "pt/core/utils/ipc/rmq/utils.h"
-#include "pt/core/utils/threads/CPUManager.h"
+#include <utils/ipc/rmq/utils.h>
+#include <utils/threads/CPUManager.h>
 #include <iostream>
-#include <pt/core/utils/threads/Worker.h>
+#include <utils/threads/Worker.h>
 
-#include <pt/core/utils/ipc/rmq/Publisher.hpp>
-#include <pt/core/utils/ipc/rmq/Consumer.hpp>
+#include <utils/ipc/rmq/Publisher.hpp>
+#include <utils/ipc/rmq/Consumer.hpp>
+#include <modules/math/SumAggregator.hpp>
 
 int main() {
+    std::cout << "Hello World!" << std::endl;
+
     while (true) {
         pt::ipc::rmq::publish_message_to_queue<std::string>("Hello World", "pt_test");
         std::this_thread::sleep_for(std::chrono::seconds(1));
