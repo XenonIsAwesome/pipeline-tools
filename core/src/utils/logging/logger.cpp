ba23e9eeeb3939c93509b2b044a53a3283736b3d
Xenon <35598885+XenonIsAwesome@users.noreply.github.com>
2025-12-18 21:58:36 +0200

    Fix/dawg log (#9)
    * fix dawglog cmake and bug with logger init
    
    * added benchmark as a depend to the ci base image
    
    * enhance CI workflow: add GitHub Container Registry login and image push steps
    
    * refactor CI workflow: simplify image handling by removing GHCR login and push steps
    
    * add tolower
    
    * fix ci
    
    * refactor CI workflow: standardize image handling by introducing OWNER_LC variable
    
    * refactor CI workflow: streamline image handling and reintroduce OWNER_LC variable
    
    * refactor CI workflow: remove unnecessary sudo commands and streamline package installation
    
    * gtest results
    
    * revert

--- core/src/utils/logging/logger.cpp
+++ core/src/utils/logging/logger.cpp
@@ -7,31 +7,27 @@
 namespace pt::utils::logging {
 /**
  * The default location of the DawgLogger configuration file
- * (inside the home directory of the user)
+ * (inside the current directory)
  */
-static constexpr const char* default_dawglogger_config_location_in_home = "";
+static constexpr const char* default_dawglogger_config_location_in_pwd = "/config/dawglogger.json";
 
 /** The environment variable name that holds the DawgLogger configuration file location */
 static constexpr const char* dawglogger_config_location_env_key = "DAWGLOGGER_CONFIGURATION_PATH";
 }  // namespace pt::utils::logging
 
 void pt::utils::logging::initialize_dawglogger() {
-    std::string home;
     std::string path;
 
     const auto path_raw = getenv(dawglogger_config_location_env_key);
-    const auto home_raw = getenv("HOME");
-    const auto user_raw = getenv("USER");
+    const auto pwd_raw = getenv("PWD");
 
     if (path_raw != nullptr) {
         path = std::string(path_raw);
-    } else if (home_raw != nullptr) {
-        path = std::string(home_raw) + default_dawglogger_config_location_in_home;
-    } else if (user_raw != nullptr) {
-        path = "/home/" + std::string(user_raw) + default_dawglogger_config_location_in_home;
+    } else if (pwd_raw != nullptr) {
+        path = std::string(pwd_raw) + default_dawglogger_config_location_in_pwd;
     } else {
         throw std::runtime_error("$" + std::string(dawglogger_config_location_env_key) +
-                                 ", $HOME and $USER are not defined");
+                                 "and $PWD are not defined");
     }
 
     DawgLog::Logger::init(DawgLog::Config(path));
