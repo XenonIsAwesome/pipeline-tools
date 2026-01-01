561f7d5725de331eea5371a292eb533dc5017486
xenon <ofek171202@gmail.com>
2026-01-01 21:56:17 +0200

    fixed

--- main.cpp
+++ main.cpp
@@ -1,31 +1,57 @@
-#include <utils/config/config.h>
+#include "flow/Mission.hpp"
+#include "modules/math/AdditionModule.hpp"
+#include "modules/io/PrintSink.hpp"
+#include "modules/io/ConstantSource.hpp"
+
 #include <server/routes.hpp>
+#include <utils/config/config.h>
 #include <utils/logging/logger.h>
+
+class TestFixMission: public pt::flow::Mission {
+    void build() override {
+        this->add(std::tuple(
+            std::make_shared<pt::modules::ConstantSource<int>>(5)
+        ), {}, {});
+
+        this->add({}, std::tuple(
+            std::make_shared<pt::modules::AdditionModule<int, int, int>>(1)
+        ), {});
+
+        this->add({}, {}, std::tuple(
+            std::make_shared<pt::modules::PrintSink<int>>()
+        ));
+    }
+};
 
 namespace swg = pt::server::swagger;
 
 int main() {
     pt::utils::logging::initialize_dawglogger();
 
-    auto registry = pt::server::EndpointRegistry::getInstance();
+    TestFixMission m;
+    m.start();
 
-    [[maybe_unused]] static pt::server::RegisterEndpoint _register_main(
-        [](httplib::Server& svr) {
-            DECLARE_ENDPOINT(
-                svr,
-                Get,
-                "/",
-                swg::EndpointMetadata("System Status").add_response(httplib::OK_200, "OK"),
-                [](const httplib::Request& req, httplib::Response& res) {
-                    res.status = httplib::OK_200;
-                    res.set_content("true", "application/json");
-                })
-        });
+    while (true) {}
 
-    const auto host = pt::config::get("PT_API_HOST", "0.0.0.0");
-    const auto port = pt::config::get("PT_API_HOST", 8081);
+    // auto registry = pt::server::EndpointRegistry::getInstance();
 
-    registry->start(host, port);
+    // [[maybe_unused]] static pt::server::RegisterEndpoint _register_main(
+    //     [](httplib::Server& svr) {
+    //         DECLARE_ENDPOINT(
+    //             svr,
+    //             Get,
+    //             "/",
+    //             swg::EndpointMetadata("System Status").add_response(httplib::OK_200, "OK"),
+    //             [](const httplib::Request& req, httplib::Response& res) {
+    //                 res.status = httplib::OK_200;
+    //                 res.set_content("true", "application/json");
+    //             })
+    //     });
+    //
+    // const auto host = pt::config::get("PT_API_HOST", "0.0.0.0");
+    // const auto port = pt::config::get("PT_API_HOST", 8081);
+    //
+    // registry->start(host, port);
 
     return 0;
 }
